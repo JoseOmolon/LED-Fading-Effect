eb56364d302017e59b563eade89de84fdb845923
Jose <159615295+JoseOmolon@users.noreply.github.com>
2025-05-19 15:07:48 +0200

    Update LED_FadingEffect.ino

--- LED_FadingEffect.ino
+++ LED_FadingEffect.ino
@@ -8,7 +8,13 @@ void loop(){
   // Fade in
   for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5){
     analogWrite(ledPin, fadeValue);
+    delay(30);
 }
 
-
+  // Fade Out
+  for(int fadeValue = 255; fadeValue >= 0; fadeValue -= 5){
+    analogWrite(ledPin, fadeValue);
+    delay(30);
+}
+  
 }
